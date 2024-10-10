c9ef3577758e68fffaeab931a44e22ec4af14f7e
AJYoung4 <ajyoung4@live.com>
2024-10-10 16:00:07 -0400

    main: added game loop, fills screen color and updates the displaly

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import pygame
 from constants import *
 
 def main():
+    # set up pygame
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Game Loop
+    while True:
+        
+        # fill the screen black
+        screen.fill((0, 0, 0))
+        
+        # update the display
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     main()
